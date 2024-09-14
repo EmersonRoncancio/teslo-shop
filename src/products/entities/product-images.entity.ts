e3b565a97901391508b6d6fa772cc69081b9ce5b
Emerson Roncancio <roncancioavila@gmail.com>
2024-09-14 16:13:37 -0500

    Renombracion de tablas en typeORM

--- src/products/entities/product-images.entity.ts
+++ src/products/entities/product-images.entity.ts
@@ -1,7 +1,7 @@
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { Product } from './product.entity';
 
-@Entity()
+@Entity({ name: 'products' })
 export class ProductImages {
   @PrimaryGeneratedColumn()
   id: number;
