bf318b5fead0d5082fd072843d4821e91c2efd2d
Emerson Roncancio <roncancioavila@gmail.com>
2024-10-14 23:51:24 -0500

    recepcion de mensajes del cliente y respuesta del mismo servidor

--- src/web-sockets/web-sockets.gateway.ts
+++ src/web-sockets/web-sockets.gateway.ts
@@ -1,11 +1,13 @@
 import {
   OnGatewayConnection,
   OnGatewayDisconnect,
+  SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
 import { WebSocketsService } from './web-sockets.service';
 import { Socket } from 'socket.io';
+import { MessageClientDTO } from './dto/message-client.dto';
 
 @WebSocketGateway({ cors: true })
 export class WebSocketsGateway
@@ -17,13 +19,22 @@ export class WebSocketsGateway
 
   handleConnection(client: Socket) {
     this.webSocketsService.registerClient(client);
-    console.log(this.webSocketsService.getClients());
     this.wss.emit('clients-updated', this.webSocketsService.getClients());
   }
 
   handleDisconnect(client: Socket) {
     this.webSocketsService.removeCliente(client.id);
-    console.log(this.webSocketsService.getClients());
     this.wss.emit('clients-updated', this.webSocketsService.getClients());
+  }
+
+  @SubscribeMessage('pruebaCliente')
+  onMessageClient(client: Socket, message: MessageClientDTO) {
+    client.broadcast.emit('serverMessage', {
+      message: message.message,
+    });
+
+    this.wss.emit('serverMessage', {
+      message: message.message,
+    });
   }
 }
